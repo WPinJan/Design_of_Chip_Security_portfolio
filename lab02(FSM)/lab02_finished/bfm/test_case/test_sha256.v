0d11775e108e77da6052d8b3daafd54c4be70536
王品然 <wpinjan@gmail.com>
2025-09-17 14:50:10 +0800

    Add files via upload

---
+++ lab02(FSM)/lab02_finished/bfm/test_case/test_sha256.v
@@ -0,0 +1,55 @@
+//This is test_case
+initial begin
+   if($test$plusargs("test_sha256")) begin
+	   @(ev_rst_done);
+	   //read vectors from .dat
+       $readmemh("../bfm/test_case/all_vectors/test_vec.dat",test_vec);
+       $readmemh("../bfm/test_case/all_vectors/gold_vec.dat",gold_vec);
+       $readmemh("../bfm/test_case/all_vectors/msg_len.dat",msg_len_vec);
+       $readmemh("../bfm/test_case/all_vectors/msg_be.dat",msg_be_vec);
+       $readmemh("../bfm/test_case/all_vectors/msg_lst.dat",msg_lst_vec);
+
+       //load vectors into memory
+       for(int i=0;i<TX_TESTLEN;i=i+1) TX.loadmem(i,test_vec[i]);
+       for(int i=0;i<RX_TESTLEN;i=i+1) CHKR.loadmem(i,gold_vec[i]);
+       for(int i=0;i<TX_TESTLEN;i=i+1) TX.load_msgbe(i,msg_be_vec[i]);
+       for(int i=0;i<TX_TESTLEN;i=i+1) TX.load_msglst(i,msg_lst_vec[i]);
+       for(int i=0;i<TX_MSG_NUMB;i=i+1) TX.load_msglen(i,msg_len_vec[i]);
+
+       //set config
+       TX.setcfg_length(TX_TESTLEN);   //setup 傳了多少test vector之後關閉(=>idle)
+       TX.setcfg_pause(1);             //setup 要不要有hold state(valid = 0)
+       TX.setcfg_pause_rate(10);       //setup 要hold多少個cycle的最大值
+       TX.setcfg_w2_en(1);             //setup 要不要有W2 state
+       TX.setcfg_w2_rate(10);          //setup w2_cycle的最大值
+       TX.setcfg_w3_en(1);             //setup 要不要有W3 state
+       TX.setcfg_w3_rate(10);          //setup w3_cycle的最大值
+
+       RX.setcfg_length(RX_TESTLEN);
+
+       CHKR.setcfg_length(RX_TESTLEN);
+       CHKR.setcfg_check_unknown(1);
+       CHKR.setcfg_check_overflow(1);
+       CHKR.setcfg_pass_showmsg(1);//fail msg is always shown
+       CHKR.setcfg_fail_stopsim(1);//fail then call $finish
+
+       //start the test
+       fork
+          TX.start(1,10);          //delay how many cycles then start bfm
+          RX.start(1);
+          CHKR.start(1);
+       join
+
+       //wait done
+       @(posedge clk);
+       while (tx_busy | rx_busy)begin//check bfm busy status
+          @(posedge clk);
+       end 
+      
+       ->ev_test_done;
+                  
+
+   end
+end
+
+
