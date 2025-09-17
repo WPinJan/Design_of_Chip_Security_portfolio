30e7f031d2fb700eac49b698cf68b758835a756e
王品然 <wpinjan@gmail.com>
2025-09-17 15:02:02 +0800

    Add files via upload

---
+++ lab02(FSM)/lab02_finished/sim/sim.f
@@ -0,0 +1,20 @@
+///////////////
+// Waveform
+///////////////
++define+USE_FSDB
++FSDB
+
+//+define+USE_VCD
+//+VCD
+
++incdir+../bfm/
++incdir+../bfm/test_case/
++incdir+../rtl/
++incdir+../tb/
+//
+//+define+USE_DPI
++test_sha256
+///////////////
+// Files
+///////////////
+file.v
